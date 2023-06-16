3be850ecc7f54e95ae1e97ff7434971d2a0be2e8
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-16 09:57:40 +0800

    add mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+require('dotenv').config()
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+const authRoutes = require('./routes/authRoutes')
 
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 const port = process.env.PORT || 8082
 
 const startServer = async () => {
     try {
+        connectDB();
         app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
         app.get('/', (req, res) => res.send("hello backend!"));
     } catch (error) {
