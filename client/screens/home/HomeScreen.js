1173e73e28ddbef43001e8ba4eb6fe067cfc9dea
Khulon <88884346+Khulon@users.noreply.github.com>
2023-06-16 16:51:57 +0800

    home page

--- client/screens/home/HomeScreen.js
+++ client/screens/home/HomeScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, Image, TouchableOpacity, Alert} from 'react-nat
 import React, { useEffect, useState} from "react";
 import useAuth from '../../hooks/useAuth';
 import BottomNavigator from '../../components/BottomNavigation';
+import { Ionicons } from "react-native-vector-icons";
 
 export default function HomeScreen({ navigation }) {
 
@@ -28,13 +29,40 @@ export default function HomeScreen({ navigation }) {
 
     },
     topTab: {
-      borderWidth:1,
       backgroundColor:'#D9D9D9',
-      alignItems: 'center',
-      justifyContent: 'center',
+      flexDirection:'row',
+      alignItems: 'flex-end',
+      justifyContent: 'space-between',
       width:'100%',
       height:100
     },
+    defaultCard:{
+      height:'100%',
+      backgroundColor:'white',
+      borderRadius:30,
+      shadowColor: '#000',
+      shadowColor: '#000',
+      shadowOffset: { width: 1, height: 2 },
+      shadowOpacity: 0.5,
+      shadowRadius: 2,
+      elevation: 2,
+
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    headerText:{
+      paddingLeft:'7%',
+      paddingBottom:'1%',
+      fontWeight:800,
+      fontSize:28, 
+      color:'white',
+      shadowColor: '#000',
+      shadowColor: '#000',
+      shadowOffset: { width: 1, height: 1 },
+      shadowOpacity: 0.5,
+      shadowRadius: 1,
+      elevation: 1,
+    }
   });
 
   const { logoutUser } = useAuth();
@@ -51,11 +79,31 @@ export default function HomeScreen({ navigation }) {
       <View style={styles.pageHome}>
 
         <View style={styles.topTab}>
-          <Text onPress={()=>handleLogout()} style={styles.text}>Logout</Text>
+
+          <Text style={styles.headerText}>
+            Welcome
+          </Text>
+
+          <View style={{paddingRight:'7%', paddingBottom:'1%',}}>
+            <TouchableOpacity onPress={()=>handleLogout()} style={{width:40, height:40, borderRadius:20, backgroundColor:'#C2C4CA', alignItems:'center', justifyContent:'center'}}>
+              <Ionicons name="person" color="#444" size='30px'/>
+            </TouchableOpacity>
+          </View>
         </View>
 
-        <View style={{width:'100%', flexGrow:1, backgroundColor:'#D9D9D9', borderWidth:1}}>
-
+        <View style={{width:'100%', flexGrow:1, backgroundColor:'#D9D9D9', alignItems:'center', justifyContent:'center'}}>
+          <View style={{width:'100%', height:'100%', position:'absolute', justifyContent:'space-evenly'}}>
+            <View style={{height:'84%', justifyContent:'center', alignItems:'center'}}>
+              <View style={[styles.defaultCard, {width:'90%'}]}>
+                <Text>lol</Text>
+              </View>
+            </View>
+            <View style={{height:'12%', justifyContent:'center', alignItems:'center'}}>
+              <View style={[styles.defaultCard, { width:'60%'}]}>
+                <Text>lol</Text>
+              </View>
+            </View>
+          </View>
         </View>
 
       </View>
