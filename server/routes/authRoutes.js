8f9d3bf60b450de1a901b3cce6dafaae47e41858
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-16 09:57:50 +0800

    add auth routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { signIn, signUp } = require('../controller/authController')
+
+const router = express.Router()
+
+router.post('/signin', signIn)
+
+router.post('/signup', signUp)
+
+module.exports = router;
