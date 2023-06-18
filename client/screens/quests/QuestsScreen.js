97f8c082d6f90a1636a860fb0c01cd1222c2b229
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-19 01:59:54 +0800

    add status to quests

--- client/screens/quests/QuestsScreen.js
+++ client/screens/quests/QuestsScreen.js
@@ -3,8 +3,53 @@ import React from "react";
 import BottomNavigator from '../../components/BottomNavigation';
 
 export default function QuestsScreen({ navigation }) {
+    const convertTZ = (date) => {
+        return ((typeof date === "string" ? new Date(date) : date).toLocaleString('en-US', {timezone: 'Asia/Singapore'}))
+    }
+    
+    const convertDate = (timestamp) => {
+        const time = convertTZ(timestamp)
+        console.log("convert", time)
+        console.log(time)
+        return time
+        // const day = timestamp.getDay()
+        // let dotw = ""
+        // const date = timestamp.getDate()
+        // const month = timestamp.getMonth()+1
+        // const hours = timestamp.getHours()
+        // let hr = ""
+        // const minutes = timestamp.getMinutes()
+        
+        // if (hours < 10) {
+        //     hr = "0" + hours
+        // } else {
+        //     hr = hours
+        // }
+
+        // switch (day) {
+        // case 0: dotw = "Sun"
+        //     break
+        // case 1: dotw = "Mon"
+        //     break
+        // case 2: dotw = "Tue"
+        //     break
+        // case 3: dotw = "Wed"
+        //     break
+        // case 4: dotw = "Thu"
+        //     break
+        // case 5: dotw = "Fri"
+        //     break
+        // case 6: dotw = "Sat"
+        //     break
+        // default: dotw = "invalid"
+        //     break
+        // }
+
+        // return dotw + " " + date + "/" + month + ", " + hr + minutes + "hrs"
+    }
 
     const renderQuests = ({ item }) => {
+        const currentTime = new Date()
         const isClickable = (type) => {
             switch (type) {
             case 'daily':
@@ -28,8 +73,54 @@ export default function QuestsScreen({ navigation }) {
         const openPassword = () => {
             navigation.navigate('PasswordModal', { id: item.id, points: item.points})
         }
+
+        const Remark = ({ status, startTime, endTime }) => {
+            
+            console.log("remark")
+            console.log(currentTime)
+            console.log(startTime)
+            const start = new Date(startTime)
+            const end = new Date(endTime)
+            let text = ""
+            let state = ""
+
+            if (status === "completed") {
+                text = "Done!"
+                state = "Completed"
+            } 
+            if (status === "incomplete") {
+                if (currentTime < start) {
+                    //not started: startdate not reached
+                    state = "Not Started"
+                    text = "Starting on: " + convertDate(start)
+                } else if (start <= currentTime < end) {
+                    //ongoing: startdate passed but not enddate
+                    state = "Ongoing"
+                    text = "Expires: " + convertDate(end)
+                } else if (end <= currentTime) {
+                    //failed: enddate passed
+                    state = "Failed"
+                    text = "Expired: " + convertDate(end)
+                } else {
+                    state = "unknown"
+                    text= "unknown"
+                }
+            }
+
+
+
+            return (
+                <View>
+                    <Text style={{ color: 'gray'}}>{text}</Text>
+                    <View style={{ marginTop: 4, borderRadius: 10, backgroundColor: '#D9D9D9', padding: 4, width:'30%', justifyContent:'center', alignItems:'center'}}>
+                        <Text style={{ color: 'gray'}}>{state}</Text>
+                    </View>
+                </View>
+            )
+        }
+
         return (
-            <TouchableOpacity style={styles.listcard} onPress={()=>isClickable(item.type)}>
+            <TouchableOpacity style={styles.listcard} onPress={()=>isClickable(item.type)} disabled={item.status === "completed" || currentTime < new Date(item.startDate) || currentTime >= new Date(item.endDate)}>
                 <View style={{ width:'95%'}}>
                     <View>
                         <View style={{ flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
@@ -42,10 +133,8 @@ export default function QuestsScreen({ navigation }) {
                         
                     </View>
                     
-                    <Text style={{ color: 'gray'}}>{item.desc}</Text>
-                    <View style={{ marginTop: 4, borderRadius: 10, backgroundColor: '#D9D9D9', padding: 4, width:'30%', justifyContent:'center', alignItems:'center'}}>
-                        <Text style={{ color: 'gray'}}>{item.status}</Text>
-                    </View>
+                    <Text style={{ color: 'black'}}>{item.desc}</Text>
+                    <Remark status={item.status} startTime={item.startDate} endTime={item.endDate}/>
                 </View>
             </TouchableOpacity>
         )
@@ -148,40 +237,40 @@ const DUMMY = [
         id: 2,
         title: 'Weekly Quiz',
         desc: 'How well do you know us',
-        status: 'ongoing',
+        status: 'incomplete',
         points: 600,
-        startDate: '',
-        endDate: '',
+        startDate: '2023-06-15T17:10:28.132Z',
+        endDate: '2023-06-20T17:10:28.132Z',
         type: 'quiz'
     },
     {
         id: 3,
         title: 'Daily Drop',
         desc: 'Scan the QR code at the pantry',
-        status: 'ongoing',
+        status: 'incomplete',
         points: 300,
-        startDate: '',
-        endDate: '',
+        startDate: '2023-06-21T17:10:28.132Z',
+        endDate: '2023-06-21T19:10:28.132Z',
         type: 'qr'
     },
     {
         id: 4,
         title: 'On Time',
         desc: 'Come to work on time',
-        status: 'failed',
+        status: 'incomplete',
         points: 400,
-        startDate: '',
-        endDate: '',
+        startDate: '2023-06-17T17:10:28.132Z',
+        endDate: '2023-06-19T17:10:28.132Z',
         type: 'qr'
     },
     {
         id: 5,
         title: 'Company Event',
         desc: 'Bonding at ORTO',
-        status: 'ongoing',
+        status: 'incomplete',
         points: 1000,
-        startDate: '',
-        endDate: '',
+        startDate: '2023-06-20T17:10:28.132Z',
+        endDate: '2023-06-24T17:10:28.132Z',
         type: 'qr'
     },
 ]
