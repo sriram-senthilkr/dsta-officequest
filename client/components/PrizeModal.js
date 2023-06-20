11baf58b8399489d913500b6882e7996846d5044
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-20 19:06:13 +0800

    add images to prize modal

--- client/components/PrizeModal.js
+++ client/components/PrizeModal.js
@@ -1,4 +1,4 @@
-import { View, Text, Modal, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Image, Modal, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
 
@@ -8,7 +8,25 @@ import React from 'react'
 //prize: prize to show (string)
 //prizetype: "gacha" or "others"
 const PrizeModal = ({ visible, closeModal, prize, prizeType }) => {
-    const gacha = (prizeType === 'gacha') ? (prize + " to replace with gacha") : prize
+    const map = [
+        {key: 0, name:'Cheeseburger', description:'You are what you eat', image:require('../assets/cheeseburger.png')},
+        {key: 1, name:'coffee', description:'Drink me!', image:require('../assets/coffee.png')},
+        {key: 2, name:'ice cream', description:'Cold Cold Cold', image:require('../assets/ice_cream.png')},
+        {key: 3, name:'microwave', description:'I cook food!', image:require('../assets/microwave.png')},
+        {key: 4, name:'onigiri', description:'What did the rice ball say to the seaweed? Im onigiri-nally yours!', image:require('../assets/onigiri.png')},
+        {key: 5, name:'salmon', description:'Salmon-nella...', image:require('../assets/salmon_maki.png')},
+        {key: 6, name:'soda', description:'Coke? Or Pepsi...', image:require('../assets/soda.png')},
+        {key: 7, name:'vending machine', description:'Dorameon!', image:require('../assets/vending_machine.png')},
+        {key: 8, name:'toaster', description:'Better then microwave', image:require('../assets/toaster.png')},
+        {key: 9, name:'hotdog', description:'Hot dwagg!', image:require('../assets/hotdog.png')},
+    ]
+
+    // const gacha = (prizeType === 'gacha') ? (
+    //     map.filter(obj=>obj.key === prize).image
+    // ) : prize
+
+    
+
     return (
         <View style={styles.back}>
             <Modal
@@ -23,7 +41,12 @@ const PrizeModal = ({ visible, closeModal, prize, prizeType }) => {
                     <View style={styles.modalView}>
                         <Text style={{fontWeight: 600, fontSize: 40, marginBottom:20}}>Congrats!</Text>
                         <Text style={{fontSize: 20, marginBottom: 20}}>You received:</Text>
-                        <Text style={styles.modalText}>{gacha}</Text>
+                        <Text style={styles.modalText}>{prize}</Text>
+                        {/* <Image
+                            style={{width:45, height:45}}
+                            source={map.filter(obj=>obj.key === prize).image}
+                            resizeMode={'contain'}
+                        /> */}
                         <Pressable
                             style={[styles.button, styles.buttonClose]}
                             onPress={() => closeModal()}
@@ -36,6 +59,9 @@ const PrizeModal = ({ visible, closeModal, prize, prizeType }) => {
         </View>
     )
 }
+
+
+
 const styles = StyleSheet.create({
     centeredView: {
         flex: 1,
