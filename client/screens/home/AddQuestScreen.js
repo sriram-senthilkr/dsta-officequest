c6f2ace082e1b73ee92f14c484ddc25e478b2b47
Benjamin Toh <toh.benjamin123@gmail.com>
2023-06-21 14:10:06 +0800

    fix bugs

--- client/screens/home/AddQuestScreen.js
+++ client/screens/home/AddQuestScreen.js
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
         minWidth: 330,
         backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'start',
+        justifyContent: 'flex-start',
         fontFamily: "Arial",
         paddingTop: 40,
         padding: 20
